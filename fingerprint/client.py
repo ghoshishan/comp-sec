3560aeedc484000909fdcf2b94f268b6616b8819
Ishan Ghosh <ghosh.ishang@gmail.com>
2021-03-15 17:15:10 +0530

    Move auth history client function from server

--- fingerprint/client.py
+++ fingerprint/client.py
@@ -11,7 +11,7 @@ class Client():
     def generate_verification_code(self):
         user_vcode = random.sample(range(1, 255), 4)
         return user_vcode
-    
+
     def enrollment_transform(self, user_fingerprint, user_vcode):
         transformed_fingerprint = user_fingerprint + user_vcode
         sumOfXiSquare = sum(x*x for x in user_fingerprint)
@@ -21,14 +21,14 @@ class Client():
 
     def paillier_encrypt(self, pub_key, plaintxt):
         pass
-        
+
     def paillier_decrypt(self, priv_key, cipher):
-        pass 
+        pass
 
     def paillier_encrypt_vector(self, pub_key, transformed_fingerprint):
         # Return component wise encrypted vector
         pass
-    
+
     def store_credentials(self, user_roll_no, user_pin, user_tid, user_key_pair, user_vcode):
         return database.store_credentials(user_roll_no, user_pin, user_tid, user_key_pair, user_vcode)
 
@@ -36,14 +36,22 @@ class Client():
         return database.retrieve_credentials(user_roll_no, user_pin)
 
     def verification_transform(self,user_fingerprint, user_vcode):
-        # is not this same as enrollment_transform 
+        # is not this same as enrollment_transform
         transformed_fingerprint = user_fingerprint + user_vcode
-        transformed_fingerprint = [ -2*n for n in transformed_fingerprint] 
+        transformed_fingerprint = [ -2*n for n in transformed_fingerprint]
         sumOfYiSquare = sum(y*y for y in user_fingerprint)
         sumOfViSquare = sum(v*v for v in user_vcode)
         transformed_fingerprint.extend([sumOfYiSquare, sumOfViSquare,1, 1])
         return transformed_fingerprint
 
+    def mark_authentication(self, user_roll_no):
+        # Record the timestamp of authentication
+        db.mark_authentication(user_roll_no)
+        return
+
+    def get_auth_history(self, user_roll_no):
+        # Return array of timestamps at which user has authenticated
+        return db.get_auth_history(user_roll_no)
 
 
-    
+
